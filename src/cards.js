3f3c3ee3e101fd4768db2621748ede9a20489204
Ruszka Tamas <ruszka.tamas@gmail.com>
2021-03-07 20:31:51 +0100

    Adding javascript functionality and refactoring the design

---
+++ src/cards.js
@@ -0,0 +1,14 @@
+const cards = [
+	{
+		category: "paint",
+		topic: "cat",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "dog",
+		point: 3
+	}
+];
+
+export { cards };
---
+++ src/main.js
@@ -0,0 +1,64 @@
+import { cards } from './cards.js';
+
+const pantomimeCards = cards.filter(({ category }) => category == 'pantomime');
+const describeCards = cards.filter(({ category }) => category == 'describe');
+const paintCards = cards.filter(({ category }) => category == 'paint');
+
+function pickCard(sourceList) {
+	const index = Math.random(sourceList.length);
+
+	return sourceList.splice(index, 1)[0];
+}
+
+const categoryButtonElements = document.querySelectorAll('.btn.category');
+const googleSearchElement = document.querySelector('#google-search');
+const cardCategoryElement = document.querySelector('.card-details > .card-category');
+const cardTopicElement = document.querySelector('.card-details > .card-topic');
+const cardPointElement = document.querySelector('.card-details > .card-point');
+const cardCountValueElement = document.querySelector('.card-count > .statistic-value');
+const lastTopicValueElement = document.querySelector('.last-topic > .statistic-value');
+const lastCategoryValueElement = document.querySelector('.last-category > .statistic-value');
+
+[ ...categoryButtonElements ].forEach((btn) => {
+	btn.addEventListener('click', () => {
+		const category = btn.id;
+		let sourceList;
+
+		switch(category) {
+			case 'pantomime':
+				sourceList = pantomimeCards;
+				break;
+			case 'describe':
+				sourceList = describeCards;
+				break;
+			case 'paint':
+				sourceList = paintCards;
+				break;
+			default:
+				console.error(`Unknown category type: ${category}`);
+		}
+
+		if (sourceList.length === 0) {
+			window.alert(`Oops, no more elements in the ${category} category, it's time to refresh your browser!`)
+
+			return;
+		}
+
+		const newCard = pickCard(sourceList);
+
+		lastCategoryValueElement.innerHTML = cardCategoryElement.innerHTML;
+		lastTopicValueElement.innerHTML = cardTopicElement.innerHTML;
+
+		cardCategoryElement.innerHTML = category;
+		cardTopicElement.innerHTML = newCard.topic;
+		cardPointElement.innerHTML = newCard.point;
+
+		cardCountValueElement.innerHTML = parseInt(cardCountValueElement.innerHTML, 10) + 1;
+
+		googleSearchElement.classList.remove('hidden');
+	});
+});
+
+googleSearchElement.addEventListener('click', () => {
+	window.open(`https://www.google.com/search?q=${cardTopicElement.innerHTML}`);
+});
