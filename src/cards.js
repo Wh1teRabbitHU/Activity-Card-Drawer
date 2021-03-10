6f3f96fa905b14fb54baf60510f5a7c1ada508f4
Ruszka Tamas <ruszka.tamas@gmail.com>
2021-03-10 15:50:25 +0100

    Adding 89 new cards

--- src/cards.js
+++ src/cards.js
@@ -1,13 +1,449 @@
 const cards = [
 	{
 		category: "paint",
-		topic: "cat",
+		topic: "Elephant calf",
+		point: 6
+	},
+	{
+		category: "paint",
+		topic: "Beer foam",
 		point: 3
 	},
 	{
 		category: "paint",
-		topic: "dog",
+		topic: "Bird nest",
+		point: 5
+	},
+	{
+		category: "paint",
+		topic: "Race car driver",
+		point: 4
+	},
+	{
+		category: "paint",
+		topic: "Sunglasses",
 		point: 3
+	},
+	{
+		category: "paint",
+		topic: "Greeting card",
+		point: 5
+	},
+	{
+		category: "paint",
+		topic: "Table tenis",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "UFO",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "Elephant seal",
+		point: 6
+	},
+	{
+		category: "paint",
+		topic: "Bank (building)",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "Cucumber",
+		point: 6
+	},
+	{
+		category: "paint",
+		topic: "Rectangle",
+		point: 5
+	},
+	{
+		category: "paint",
+		topic: "Painting",
+		point: 6
+	},
+	{
+		category: "paint",
+		topic: "Book week",
+		point: 5
+	},
+	{
+		category: "paint",
+		topic: "Boulder",
+		point: 4
+	},
+	{
+		category: "paint",
+		topic: "Straw bag",
+		point: 5
+	},
+	{
+		category: "paint",
+		topic: "Train driver",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "Swimming pool",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "Hanging rope",
+		point: 4
+	},
+	{
+		category: "paint",
+		topic: "Gas latern",
+		point: 4
+	},
+	{
+		category: "paint",
+		topic: "Spine",
+		point: 5
+	},
+	{
+		category: "paint",
+		topic: "Wooden leg",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "Apple seed",
+		point: 3
+	},
+	{
+		category: "paint",
+		topic: "Golden rain",
+		point: 4
+	},
+	{
+		category: "paint",
+		topic: "Snow white",
+		point: 6
+	},
+	{
+		category: "paint",
+		topic: "Screw washer",
+		point: 5
+	},
+	{
+		category: "paint",
+		topic: "Forrest",
+		point: 4
+	},
+	{
+		category: "paint",
+		topic: "Bare foot",
+		point: 6
+	},
+	{
+		category: "paint",
+		topic: "Seat belt",
+		point: 4
+	},
+	{
+		category: "paint",
+		topic: "Car race",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "highway",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Wooden cabin",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Lead soldier",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Toast",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Lead singer",
+		point: 3
+	},
+	{
+		category: "describe",
+		topic: "Courier",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Water mill",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Guilt",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Lunch break",
+		point: 3
+	},
+	{
+		category: "describe",
+		topic: "Post office",
+		point: 3
+	},
+	{
+		category: "describe",
+		topic: "Love",
+		point: 3
+	},
+	{
+		category: "describe",
+		topic: "Side road",
+		point: 3
+	},
+	{
+		category: "describe",
+		topic: "Translation",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Fruit salad",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Beauty spot",
+		point: 5
+	},
+	{
+		category: "describe",
+		topic: "Watchtower",
+		point: 3
+	},
+	{
+		category: "describe",
+		topic: "Throat",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Fairy tale",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Vegetable",
+		point: 3
+	},
+	{
+		category: "describe",
+		topic: "Balance point",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Holy text",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Engineer",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Illusion",
+		point: 6
+	},
+	{
+		category: "describe",
+		topic: "Anesthesia",
+		point: 6
+	},
+	{
+		category: "describe",
+		topic: "Gold dust",
+		point: 5
+	},
+	{
+		category: "describe",
+		topic: "Record attempt",
+		point: 4
+	},
+	{
+		category: "describe",
+		topic: "Voice pitch",
+		point: 5
+	},
+	{
+		category: "describe",
+		topic: "Ideology",
+		point: 6
+	},
+	{
+		category: "pantomime",
+		topic: "Jester",
+		point: 6
+	},
+	{
+		category: "pantomime",
+		topic: "Mother",
+		point: 3
+	},
+	{
+		category: "pantomime",
+		topic: "Post box",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Coughing",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Emergency break",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Half circle",
+		point: 4
+	}
+	,
+	{
+		category: "pantomime",
+		topic: "Camel fur",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Similarity",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Round",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Bird cage",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Tooth ache",
+		point: 3
+	},
+	{
+		category: "pantomime",
+		topic: "Baking oven",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Firearm",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Violin",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Shivering",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Monkey",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Bread",
+		point: 3
+	},
+	{
+		category: "pantomime",
+		topic: "Movie star",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Torture bench",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Engine",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Hand break",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Soap buble",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Couch",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Champagne bottle",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Round trip",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Nose cavity",
+		point: 3
+	},
+	{
+		category: "pantomime",
+		topic: "Apple juice",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "High school",
+		point: 6
+	},
+	{
+		category: "pantomime",
+		topic: "Rubber ball",
+		point: 5
+	},
+	{
+		category: "pantomime",
+		topic: "Running shoes",
+		point: 4
+	},
+	{
+		category: "pantomime",
+		topic: "Horse riding",
+		point: 5
 	}
 ];
 
